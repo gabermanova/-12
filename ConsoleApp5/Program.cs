8ef015bd0a0a74b6eae31734df2991167b31b845
Екатерина_Габерманов <_@DESKTOP-M91PJSA>
2022-12-02 17:59:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        int[] arr = { 8, 3, 5, -1, 2 };
+
+        Array.Sort(arr);
+        if (arr.Length > 0)
+        {
+            Console.WriteLine("Минимальное число из последовательности чисел " + arr[0]);
+            Console.ReadKey();
+        }
+    }
+}
